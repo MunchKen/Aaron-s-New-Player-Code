be51a436e231067bd54bbd784dd3731e7812711c
MunchKen <116809214+MunchKen@users.noreply.github.com>
2022-11-13 23:08:12 +0800

    Create Enemy.gd

---
+++ Enemy.gd
@@ -0,0 +1,34 @@
+extends Character
+class_name Enemy , "res://Art/v1.1 dungeon crawler 16X16 pixel pack/v1.1 dungeon crawler 16X16 pixel pack/enemies/flying creature/fly_anim_f1.png"   #class name are used to be able to use the script as a node
+
+var path: PoolVector2Array
+
+onready var navigation: Navigation2D = get_parent().current_scene.get_node("Navigation2D")
+
+onready var player: KinematicBody2D= get_tree().current_scene.get_node("Player")
+
+onready var path_timer: Timer = get_node("PathTimer")
+
+func chase() -> void: 
+	if path: 
+		var vector_to_next_point: Vector2 = path [0] - global_position
+		var distance_to_next_point: float = vector_to_next_point.length()
+		
+		if distance_to_next_point < 2: #if you want to move enemy fast have great number than 1
+			path.remove(0)
+			if not path: 
+				return
+		mov_direction=vector_to_next_point
+		
+		if vector_to_next_point. x>0 and animated_sprite.flip_h: 
+			animated_sprite.flip_h = false
+		elif vector_to_next_point. x<0 and not animated_sprite.flip_h: 
+			animated_sprite.flip_h = true
+
+func _on_PathTimer_timeout() -> void:
+	if is_instance_valid(player):
+		path=navigation.get_simple_path(global_position,player.position)
+	else: 
+		path_timer.stop () 
+		path= [0]  #path to an empty array and the movement direction to 0 to stop enemy
+		mov_direction = Vector2.ZERO
